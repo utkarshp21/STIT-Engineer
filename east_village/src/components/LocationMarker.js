042147d67674d13028b51db7c077efd8abf2cc00
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2018-09-02 18:50:13 -0400

    added user location marker

--- east_village/src/components/LocationMarker.js
+++ east_village/src/components/LocationMarker.js
@@ -1,36 +1,32 @@
 import React from "react";
 import {google_api_key} from '../credentials';
-
 import { withScriptjs, withGoogleMap, GoogleMap, Marker,Polyline } from "react-google-maps"
 
+import user_icon from '../assests/circle-solid.svg';
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   {  
     var user_location ={
         lat: props.location.user.latitude,
         lng: props.location.user.longitude
-    }
+    };
     var event_location = {
         lat: props.location.event.latitude,
         lng: props.location.event.longitude
-    }
-   
+    };
+
     return <GoogleMap defaultZoom={10} defaultCenter={{ lat: event_location.lat, lng:event_location.lng }}>
-        {props.isMarkerShown && <Marker position={{ lat: event_location.lat, lng:event_location.lng }} />}
-        {props.isMarkerShown && <Marker position={{ lat: user_location.lat, lng: user_location.lng }} />}
+        {props.isMarkerShown && <Marker position={{ lat: event_location.lat, lng:event_location.lng }} title="Event"  />}
+        {props.isMarkerShown && <Marker position={{ lat: user_location.lat, lng: user_location.lng }} icon={user_icon} title="Your Location" />}
         <Polyline 
             path = {
                 [user_location, event_location]
             }
             options={{ 
-            strokeColor: '#00ffff',
-            strokeOpacity: 1,
-            strokeWeight: 2,
-            icons: [{ 
-                icon: "hello",
-                offset: '10',
-                repeat: '10px'
-            }],
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
             }}
         />
     </GoogleMap>}
